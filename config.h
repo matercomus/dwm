a33785e675cd11ab3eb7e86066578d1a5ff6e980
matercomus <matek.kedzia@gmail.com>
2021-12-20 17:27:44 +0100

    added gaps, changed main font

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 20;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -69,7 +69,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *web[] = { "chromium", NULL };
+static const char *web[] = { "vivaldi-stable", NULL };
 static const char *audio[] = { "pavucontrol", NULL };
 static const char *clipmenu[] = { "clipmenu", "-fn", dmenufont, NULL };
 static const char scratchpadname[] = "scratchpad";
